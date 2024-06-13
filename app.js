f8e618a861dafc42275cf6c7beb2009ca4be9b86
rngnnn <rngnnnclk@gmail.com>
2024-06-13 09:16:04 +0200

    CHOISE

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-let yourChoise=document.getElementById("your-choice")
-const pcChoise=document.getElementById("pc-choice")
+let yourChoiseDiv=document.getElementById("your-choice")
+const pcChoiseDiv=document.getElementById("pc-choice")
+
+//& Colors
+const YELLOW = "#ffc538";
+const RED = "#fb778b";
+const GREEN = "#5ab7ac";
+
+//& Variables
+const userSelectImg=document.createElement("img")
+let userSelection;
+
+//Selection
+
+const selectionArticle=document.querySelector(".selection")
+
+selectionArticle.addEventListener("click",(e)=>{
+
+    userSelection=e.target.id
+
+if(condition)
+
+    userSelectImg.src=`./assets/${userSelection}.png`
+    yourChoiseDiv.appendChild(userSelectImg)
+})
